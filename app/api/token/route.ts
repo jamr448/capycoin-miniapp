aef9fd43e63e164e74ad024762f5a2e3e7f1dbd0
jamr448 <71365766+jamr448@users.noreply.github.com>
2026-03-30 15:51:51 -0300

    app capycoin contrnido Acerca de

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -5,13 +5,13 @@ export async function GET() {
 try{
 
 const res = await fetch(
-"https://worldscan.org/api?module=token&action=tokeninfo&contractaddress=0xe55BA4Ea7835c221a521e43BA05bC1a9508928B2"
+"https://worldscan.org/api?module=token&action=tokenholdercount&contractaddress=0xe55BA4Ea7835c221a521e43BA05bC1a9508928B2"
 );
 
 const data = await res.json();
 
 return NextResponse.json({
-holders:data.result?.holders ?? 0
+holders: Number(data.result) || 0
 });
 
 }catch(err){
